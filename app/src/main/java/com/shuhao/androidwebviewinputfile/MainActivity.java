18207dd8a9ee97d1c1d046fa6436945eeb5f40a4
luke <luke.w@topskyweb.com>
2017-08-26 10:45:16 +0800

    [update] 修改module版本号

--- app/src/main/java/com/shuhao/androidwebviewinputfile/MainActivity.java
+++ app/src/main/java/com/shuhao/androidwebviewinputfile/MainActivity.java
@@ -2,13 +2,17 @@ package com.shuhao.androidwebviewinputfile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.shuhao.webchromeclient.FileChooserWebChromeClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    FileChooserWebChromeClient fileChooserWebChromeClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        fileChooserWebChromeClient = new FileChooserWebChromeClient();
     }
 }
